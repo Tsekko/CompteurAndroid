a438e1e0c8820b5de3e093a20246a16f04c29f3f
Tsekko <maxime.kervadec@laposte.net>
2021-03-16 09:39:45 +0100

    Création méthodes agissant sur le compteur

--- app/src/main/java/com/example/compteur/SecondFragment.java
+++ app/src/main/java/com/example/compteur/SecondFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
-
+    private int count = 0;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -23,12 +24,35 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+        view.findViewById(R.id.button_second).setOnClickListener(view1 -> NavHostFragment.findNavController(SecondFragment.this)
+                .navigate(R.id.action_SecondFragment_to_FirstFragment));
+
+        view.findViewById(R.id.minusButton).setOnClickListener(view1 -> minusCount());
+
+        view.findViewById(R.id.plusButton).setOnClickListener(view1 -> plusCount());
+
+        view.findViewById(R.id.RAZButton).setOnClickListener(view1 -> razCount());
+    }
+
+    private void minusCount(){
+        if(count>0){
+            count--;
+        }
+        modifyTextView(count);
+    }
+
+    private void plusCount(){
+        count++;
+        modifyTextView(count);
+    }
+
+    private void razCount(){
+        count=0;
+        modifyTextView(count);
+    }
+
+    private void modifyTextView(int count){
+        TextView t = getView().findViewById(R.id.countText);
+        t.setText(count);
     }
 }
